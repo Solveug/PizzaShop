c44c0c53cecbaa0e68f578cf94373c034eeb154f
Aleksandra <solveug@rambler.ru>
2021-04-26 19:06:22 +0300

    bug fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_26_150212) do
+ActiveRecord::Schema.define(version: 2021_04_26_152614) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
