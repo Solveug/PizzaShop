4387f17e9411f4ecfb90f71045b8b8248b16991a
Aleksandra <solveug@rambler.ru>
2021-04-27 14:53:14 +0300

    display products, update migration code

--- db/migrate/20210426152614_add_products.rb
+++ db/migrate/20210426152614_add_products.rb
@@ -1,7 +1,36 @@
 class AddProducts < ActiveRecord::Migration[6.1]
   def change
-    Product.create :title => 'Margarita', :description => 'Classic', :price => 550, :size => 40, :is_spicy => false, :is_veg => false, :is_best_offer => false, :path_to_image => '/images/mar.jpg'
-    Product.create :title => 'Pepperoni', :description => 'Nice Pepperoni pizza', :price => 450, :size => 40, :is_spicy => true, :is_veg => false, :is_best_offer => true, :path_to_image => '/images/peperoni.jpg'
-    Product.create :title => 'Vegetarian', :description => 'Amazing Vegetarian pizza', :price => 400, :size => 40, :is_spicy => false, :is_veg => true, :is_best_offer => false, :path_to_image => '/images/veg.jpg'
+    Product.create ({
+      :title => 'Margarita',
+      :description => 'Classic',
+      :price => 550,
+      :size => 40,
+      :is_spicy => false,
+      :is_veg => false,
+      :is_best_offer => false,
+      :path_to_image => '/images/mar.jpg'
+    })
+
+    Product.create ({
+      :title => 'Pepperoni',
+      :description => 'Nice Pepperoni pizza',
+      :price => 450,
+      :size => 40,
+      :is_spicy => true,
+      :is_veg => false,
+      :is_best_offer => true,
+      :path_to_image => '/images/peperoni.jpg'
+    })
+
+    Product.create ({
+      :title => 'Vegetarian',
+      :description => 'Amazing Vegetarian pizza',
+      :price => 400,
+      :size => 40,
+      :is_spicy => false,
+      :is_veg => true,
+      :is_best_offer => false,
+      :path_to_image => '/images/veg.jpg'
+    })
   end
 end
