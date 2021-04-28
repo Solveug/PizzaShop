7f3222b3bcbd4232e8c03de6900226f4ff15499d
Aleksandra <solveug@rambler.ru>
2021-04-28 15:29:51 +0300

    add cart_get_orders

--- public/test.js
+++ public/test.js
@@ -13,7 +13,13 @@ function add_to_cart(id)
   x = x * 1 + 1;
   window.localStorage.setItem(key, x);
 
-  alert('Items in your cart: ' + cart_get_number_of_items());
+  update_orders_input();
+}
+
+function update_orders_input()
+{
+  var orders = cart_get_orders();
+  $('#orders_input').val(orders);
 }
 
 function cart_get_number_of_items()
@@ -34,9 +40,9 @@ function cart_get_number_of_items()
 
 function cart_get_orders()
 {
-  var orders = 0;
+  var orders = '';
 
-  for (var i = 0; i < window.localStorage.length; i++)
+  for(var i = 0; i < window.localStorage.length; i++)
   {
     var key = window.localStorage.key(i);
     var value = window.localStorage.getItem(key);
